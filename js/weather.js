59892c64621e2b99a57e2579113ac9201dd5037d
Mayank Pratap <mayank7860@gmail.com>
2016-05-20 19:34:42 +0530

    JSON data can now be viewed in html page

--- js/weather.js
+++ js/weather.js
@@ -1,27 +1,36 @@
+$(document).ready(function(){
+
 
 
     if(navigator.geolocation){
 
       navigator.geolocation.getCurrentPosition(function(position){
-      var latitude=position.coords.latitude;
-      var longitude=position.coords.longitude;
-      var apiUrl="http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=9bb8ffb7868bb72fb575bb9cba652046";
+        var latitude=position.coords.latitude;
+        var longitude=position.coords.longitude;
+        var apiUrl="http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid=9bb8ffb7868bb72fb575bb9cba652046";
 
-      $.getJSON(apiUrl,function(json){
+        var html="";
+
+        $.getJSON(apiUrl,function(json){
 
             //console.log(JSON.stringify(json));
             //console.log(json.main.temp);
-          
-            var html="";
+
+
             html+="<div class='temp'>";
             html+="<h4>Temperature is: "+json.main.temp+" </h4>";
-            html+="</div>";
-            console.log(html);
+            html+="</div><br>";
+            html+="<div class='weather'>";
+            html+="<h4>"+json.weather[0].main+"</h4>";
+            html+="</div>"
+            //console.log(JSON.stringify(json));
+            $(".message").html(html);
 
-      });
+        });
 
-      $(".message").html(html);
 
-      });
+    });
 
-    }
+  }
+
+});
